50c20286a4975dfabcf0c26e630261413203407c
Peter Gemmell <petergemmell65@hotmail.com>
2020-11-23 11:01:12 +0000

    required packages added and script created

---
+++ stock_tracker.py
@@ -0,0 +1,9 @@
+# First we import our packages
+import numpy as np
+import pandas as pd
+
+# Next we import our Data Source
+import yfinance as yf
+
+# Next we import our package for visualising the Data
+import  plotly.graph_objs as go
