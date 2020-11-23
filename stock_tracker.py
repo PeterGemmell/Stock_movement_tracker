613006b7a8f11689a0c1b86e3c9d03d754044819
Peter Gemmell <petergemmell65@hotmail.com>
2020-11-23 11:52:23 +0000

    figure declared

--- stock_tracker.py
+++ stock_tracker.py
@@ -19,3 +19,11 @@ data['Upper Band'] = data['Middle Band'] + 1.96*data['Close'].rolling(window=21)
 data['Lower Band'] = data['Middle Band'] - 1.96*data['Close'].rolling(window=21).std()
 
 # Our next step is to plot the data and check if we can predict the market movement.
+
+
+# We declare figure
+fig = go.Figure()
+
+fig.add_trace(go.Scatter(x=data.index, y= data['Middle Band'], line=dict(color='blue', width=.7), name = 'Middle Band'))
+fig.add_trace(go.Scatter(x=data.index, y= data['Upper Band'], line=dict(color='red', width=1.5), name = 'Upper Band (Sell)'))
+fig.add_trace(go.Scatter(x=data.index, y=data['Lower Band'], line=dict(color='green', width=1.5), name = 'Lower Band (Buy)'))
